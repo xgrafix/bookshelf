289bd073424f4fb4277c92b6bdfe3601a91d14de
Ali Mirza <xgrafix@gmail.com>
2021-02-01 23:39:30 +0300

    Adding App module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { TopHeaderComponent } from './top-header/top-header.component';
 import { AppRoutingModule } from './app-routing.module';
-import { WishListComponent } from './wish-list/wish-list.component'
+import { WishListComponent } from './wish-list/wish-list.component';
 import { BookComponent } from './book/book.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { HomeComponent } from './home/home.component';
