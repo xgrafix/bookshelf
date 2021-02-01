69a6dcd7e703940391d14789606f37218a3386f9
Ali <xgrafix@gmail.com>
2021-02-01 23:26:01 +0300

    Add files via upload

---
+++ src/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { TopHeaderComponent } from './top-header/top-header.component';
+import { AppRoutingModule } from './app-routing.module';
+import { WishListComponent } from './wish-list/wish-list.component';
+import { BookComponent } from './book/book.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HomeComponent } from './home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TopHeaderComponent,
+    WishListComponent,
+    BookComponent,
+    PageNotFoundComponent,
+    HomeComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
