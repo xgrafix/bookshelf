a709bdfffa3f86af36124ef9a7c4fd9a37007c3b
Ali <xgrafix@gmail.com>
2021-02-01 23:10:47 +0300

    Add files via upload

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { HomeComponent } from './home/home.component';
+import { AppComponent } from './app.component';
+import { AppModule } from './app.module';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { TopHeaderComponent } from './top-header/top-header.component';
+import { WishListComponent } from './wish-list/wish-list.component';
+import { BookComponent } from './book/book.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'book', component: BookComponent },
+  { path: 'wish_list', component: WishListComponent },
+  { path: '**', component: PageNotFoundComponent }
+];
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
