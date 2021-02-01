59fd7c5f9dbaa98e81f3e989b4fba768ea168d82
Ali Mirza <xgrafix@gmail.com>
2021-02-01 21:19:29 +0300

    Changing to Book Shelf

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/book-depository'),
+      dir: require('path').join(__dirname, './coverage/book-shelf'),
       subdir: '.',
       reporters: [
         { type: 'html' },
