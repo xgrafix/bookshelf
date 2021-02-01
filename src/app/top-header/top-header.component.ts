a1188832d7fc1b0de08a84f6a32a7aac7152c062
Ali Mirza <xgrafix@gmail.com>
2021-02-01 22:59:23 +0300

    Commit on main

--- src/app/top-header/top-header.component.ts
+++ src/app/top-header/top-header.component.ts
@@ -1,4 +1,4 @@
-import { AppComponent } from './../app.component';
+import { AppComponent } from '../../../app.component';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
