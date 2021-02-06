64e1fa3602f8934fbf932bc35b6727bef798c6e0
Ali Mirza <xgrafix@gmail.com>
2021-02-06 10:55:15 +0300

    Creating config componenet.

---
+++ src/app/config/config.componenet.ts
@@ -0,0 +1,63 @@
+import { Component } from '@angular/core';
+import { Config, ConfigService } from './config.service';
+import { MessageService } from '../message.service';
+
+@Component({
+    selector: 'app-config',
+  templateUrl: './config.component.html',
+    providers: [ ConfigService ],
+//   styleUrls: ['./home.component.css']
+})
+
+export class ConfigComponent {
+    config: any;
+    error: any;
+    headers: any;
+
+  constructor(private configService: ConfigService) { }
+
+  clear() {
+    this.config = undefined;
+    this.error = undefined;
+    this.headers = undefined;
+  }
+
+  showConfig() {
+    this.configService.getConfig()
+      .subscribe((data: Config) => this.config = {
+          heroesUrl: data.heroesUrl,
+          textfile:  data.textfile
+      });
+  }
+  
+  showConfig_v1() {
+    this.configService.getConfig_1()
+      .subscribe((data: Config) => this.config = {
+          heroesUrl: data.heroesUrl,
+          textfile:  data.textfile
+      });
+  }
+
+  showConfig_v2() {
+    this.configService.getConfig()
+      // clone the data object, using its known Config shape
+      .subscribe((data: Config) => this.config = { ...data });
+  }
+
+  showConfigResponse() {
+    this.configService.getConfigResponse()
+      // resp is of type `HttpResponse<Config>`
+      .subscribe(resp => {
+        // display its headers
+        const keys = resp.headers.keys();
+        this.headers = keys.map(key =>
+          `${key}: ${resp.headers.get(key)}`);
+
+        // access the body directly, which is typed as `Config`.
+        this.config = { ... resp.body };
+      });
+  }
+  makeError() {
+    this.configService.makeIntentionalError().subscribe(null, error => this.error = error );
+  }
+}
