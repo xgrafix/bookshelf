63cb8a6496ef87f3f566660db1e181a481356a32
Ali Mirza <xgrafix@gmail.com>
2021-02-06 10:56:52 +0300

    Configure the routing and main app module.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,12 +7,14 @@ import { TopHeaderComponent } from './top-header/top-header.component';
 import { WishListComponent } from './wish-list/wish-list.component';
 import { BookComponent } from './book/book.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ConfigComponent } from './config/config.componenet';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent },
   { path: 'book', component: BookComponent },
   { path: 'wish_list', component: WishListComponent },
+  { path: 'config', component: ConfigComponent },
   { path: '**', component: PageNotFoundComponent }
 ];
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { TopHeaderComponent } from './top-header/top-header.component';
@@ -8,6 +9,8 @@ import { WishListComponent } from './wish-list/wish-list.component';
 import { BookComponent } from './book/book.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { HomeComponent } from './home/home.component';
+import { MessageService } from './message.service';
+import { ConfigComponent } from './config/config.componenet';
 
 @NgModule({
   declarations: [
@@ -17,12 +20,14 @@ import { HomeComponent } from './home/home.component';
     BookComponent,
     PageNotFoundComponent,
     HomeComponent,
+    ConfigComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [MessageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
