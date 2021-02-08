2a2a55e50ff2d2d7aa45ea98b76e8f01e45454d7
Ali Mirza <xgrafix@gmail.com>
2021-02-08 20:41:22 +0300

    Changing the typescript to cusom API.

--- src/app/book/book.component.ts
+++ src/app/book/book.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { BooksService } from '../books.service';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Todo } from './todo';
+import { Book } from './book';
 import { Observable } from 'rxjs';
 import { map, filter, switchMap } from 'rxjs/operators';
 @Component({
@@ -13,11 +14,14 @@ export class BookComponent {
 
   booksFullInfomation?: any;
   todos?: Observable<any>;
+  books?: Observable<any>;
   newTodos?: Observable<any>;
-  
-  readonly ROOT_URL = 'https://jsonplaceholder.typicode.com';
+  newBooks?: Observable<any>;
 
-  
+  // readonly ROOT_URL = 'https://jsonplaceholder.typicode.com';
+  readonly ROOT_URL = 'http://localhost:8000';
+
+
   // Using a service to fetch the data will be loaded on loading the page.
 
   // constructor(private books: BooksService) { 
@@ -33,8 +37,16 @@ export class BookComponent {
 
     let params = new HttpParams().set('userId', '1');
     let headers = new HttpHeaders().set('Authorization', 'auth-token')
-    this.todos = this.http.get(this.ROOT_URL + '/todos', { headers });
+    this.books = this.http.get(this.ROOT_URL + '/books', { headers });
   }
+
+  getBooks() {
+
+    // let params = new HttpParams().set('userId', '1');
+    let headers = new HttpHeaders().set('Authorization', 'auth-token')
+    this.books = this.http.get(this.ROOT_URL + '/books', { headers });
+  }
+
 
   createTodos() {
     const data: Todo = {
@@ -45,7 +57,25 @@ export class BookComponent {
     }
 
     this.newTodos = this.http.post<Todo>(this.ROOT_URL + '/todos', { data })
-    .pipe(map(todo => todo.data?.title))
+      .pipe(map(todo => todo.data?.title))
   }
-  
+
+
+  createBooks() {
+    const data: Book = {
+      isbn: '8172387128312312381',
+      title: 'Laila Wa Domna',
+      author: 'Ma Adre Menho',
+      category: 'Litrature',
+    }
+
+    console.log(BooksService.toString());
+
+
+
+    // this.newBooks = this.http.post<Book>(this.ROOT_URL + '/books', JSON.stringify(data))
+    //   .pipe(map(todo => todo))
+  }
+
+
 }
